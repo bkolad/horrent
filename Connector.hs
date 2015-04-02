ad30b0d712edcec5bf78d29cf9f4cb09bb4740b4
blazej <blazejkolad@gmail.com>
2015-04-02 21:23:28 +0200

    refactoring

--- Connector.hs
+++ Connector.hs
@@ -1,44 +1,34 @@
 {-# LANGUAGE NoMonomorphismRestriction, ScopedTypeVariables, DoAndIfThenElse #-}
 
-module Connector where
+module Connector (getPeers) where
 
-import qualified Peer as P (Peer, makePeer, myId, talk, showPeer) 
+import qualified Peer as P (Peer, makePeer, myId, showPeer) 
 import qualified BencodeParser as BP (BEncode, annouce, infoHash, parseFromFile, parseFromBS, peers)
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
 import qualified UrlEncoder as Encoder (urlEncodeVars)
 import Data.Binary.Get
 import Data.Word
-import Data.Either
-
+import qualified Data.Either as DE 
 import Network.HTTP.Base (urlEncodeVars)
 import Network.HTTP as HTTP
-import Network.Socket
-import Control.Concurrent.Async (mapConcurrently)
+import qualified Network as N
+import Control.Concurrent.Async as Async (mapConcurrently)
 import Control.Exception as E
 import Control.Applicative
 import Control.Monad.Error
-import Control.Monad.Trans
-import           Data.Char
-import Data.Bits  ( (.&.), (.|.), shiftL, shiftR )
+
 
 
 import Data.ByteString.Base16 (encode)
-
-
-
 
 type ErrorIO = ErrorT String IO
 
 liftEither = ErrorT . return
 
-
                   
 getResponseFromTracker :: String -> IO String
 getResponseFromTracker url = HTTP.simpleHTTP (HTTP.getRequest url) >>= HTTP.getResponseBody 
-
-
 
 trackerUrl :: BP.BEncode -> Either String String
 trackerUrl fromDic = do ann <- BP.annouce fromDic
@@ -66,13 +56,29 @@ getIPandPort bs = runGet get32and16b (BL.fromChunks [bs])
                                       return $ (ip, port):rest
 
 
-
-makePeer :: BC.ByteString -> HostName -> PortNumber -> IO (Either String P.Peer)
+makePeer :: BC.ByteString -> N.HostName -> N.PortNumber -> IO (Either String P.Peer)
 makePeer hash h p  = E.catch (liftM Right $ P.makePeer hash h p)
                              (\(e::SomeException) -> return . Left $ show e )
-                       
-  
-  
+
+getPeers :: String->Int -> IO (Either String [P.Peer])                             
+getPeers tracker numberOfPeers = runErrorT $ makePeers tracker numberOfPeers   
+    
+makePeers :: MonadIO m => String ->Int -> ErrorT String m [P.Peer]  
+makePeers tracker numberOfP = do content <-  liftIO $ BP.parseFromFile tracker
+                                 hash <- liftEither $ BC.pack<$>(content >>= BP.infoHash)
+                                 url <-   liftEither $ content >>= trackerUrl
+                                 resp <- (liftIO . getResponseFromTracker) url
+                                 peersBS <- liftEither $ ((BP.parseFromBS . BC.pack) resp)  >>= BP.peers
+                                 let ls =  getIPandPort peersBS
+                                 peers <- liftIO $ Async.mapConcurrently (\(h,p)->  makePeer hash (show h) (fromIntegral p)) (take numberOfP ls) 
+                                 let (_, pp) = DE.partitionEithers peers  
+                                 return pp
+
+               
+               
+               
+               
+               {-
  --showResponse :: ErrorIO [P.Peer]
 showResponse = do content <-  liftIO $ BP.parseFromFile "ubuntu.torrent"
                   hash <- liftEither $ BC.pack<$>(content >>= BP.infoHash)
@@ -80,13 +86,13 @@ showResponse = do content <-  liftIO $ BP.parseFromFile "ubuntu.torrent"
                   resp <- (liftIO . getResponseFromTracker) url
                   peersBS <- liftEither $ ((BP.parseFromBS . BC.pack) resp)  >>= BP.peers
                   let ls =  getIPandPort peersBS
-                  peers <- liftIO $ mapConcurrently (\(h,p)->  makePeer hash (show h) (fromIntegral p)) (take 5 ls) 
+                  peers <- liftIO $ Async.mapConcurrently (\(h,p)->  makePeer hash (show h) (fromIntegral p)) (take 5 ls) 
                   let (_, pp) = partitionEithers peers  
                   liftIO $ mapConcurrently P.talk pp
                   liftIO $ mapM P.showPeer pp
               
             --      return $ ls
                   
-toIO = runErrorT showResponse
+                  toIO = runErrorT showResponse -}
 
 
--- Peer.hs
+++ Peer.hs
@@ -1,71 +1,21 @@
 {-# LANGUAGE ScopedTypeVariables, DoAndIfThenElse, FlexibleInstances, UndecidableInstances #-}
-module Peer (Peer, makePeer, myId, talk, showPeer) where
+module Peer (Peer, makePeer, myId, showPeer, intFromBS, peerP, handleP, amIInterested, amIChocked, amIVirgin, bitField) where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.List as L
 import qualified Control.Exception as E
-import Data.Tuple
-import Data.Binary.Get
-import Network
-import System.IO
-import Data.Binary.Put
-import Control.Applicative
-import Control.Concurrent
-import Control.Monad
+import qualified Network as N
+import qualified System.IO as SIO
 import Data.IORef
 
 
 myId = "-TR2840-d0p22uiake0b" 
 protocol = "BitTorrent protocol"
 
-data Message = KeepAlive 
-             | Choke
-             | UnChoke
-             | Interested
-             | NotInterested
-             | Have
-             | Bitfield B.ByteString
-             | Request
-             | Piece
-             | Cancel
-             | Port 
-             | Error
-             deriving (Show)
-             
-data MessageId = NoId | Id Int             
 
-
-msgToLenAndId msg = case msg of
-                         KeepAlive     -> (0, NoId)
-                         Choke         -> (1, Id 0)
-                         UnChoke       -> (1, Id 1)
-                         Interested    -> (1, Id 2)
-                         NotInterested -> (1, Id 3)
-                         Have          -> (5, Id 4)
-                         Bitfield _    -> undefined
-                         Request       -> (13, Id 6)
-                         Piece         -> undefined
-                         Cancel        -> (13, Id 8)
-                         Port          -> (3, Id 9)
-
-lenAndIdToMsg :: (Int, MessageId, B.ByteString) -> Message 
-lenAndIdToMsg lenId = case lenId of
-                          (0, NoId, _)   -> KeepAlive
-                          (1, Id 0, bs)   -> Choke
-                          (1, Id 1, bs)   -> UnChoke
-                          (1, Id 2, bs)   -> Interested
-                          (1, Id 3, bs)   -> NotInterested
-                          (5, Id 4, bs)   -> Have
-                          (len, Id 5, bf) -> Bitfield bf
-                          (13, Id 6, bs)  -> Request
-                          (len, Id 7, bs) -> Piece 
-                          (13, Id 8, bs)  -> Cancel
-                          (3, Id 9, bs)   -> Port
-                        
  
-data Peer = Peer{ handleP :: Handle
+data Peer = Peer{ handleP :: SIO.Handle
                 , peerP :: String 
                 , amIInterested :: IORef Bool -- false
                 , amIChocked :: IORef Bool  -- true
@@ -78,72 +28,19 @@ showPeer :: Peer -> IO (String, B.ByteString)
 showPeer p= do let name = peerP p
                bf <- readIORef $ bitField p    
                return (name, bf)
-   
-
-talk :: Peer -> IO ()
-talk peer =  E.catch (talkToPeer peer) (\(e::E.SomeException) -> print $ "Failure "++(peerP peer) ++(show e) {-- TODO close the connection-} )
-
-canTalToPeer peer = do isVirgin <- readIORef (amIVirgin peer)    
-                       isInterested <-readIORef (amIInterested peer)                     
-                       isIChocked <- readIORef (amIChocked peer)
-                       return $  isVirgin || (isIChocked && isInterested)
-              
-              
-talkToPeer :: Peer -> IO ()
-talkToPeer peer = do canTalk <- canTalToPeer peer 
-                     if (canTalk) then do
-                        let handle = handleP peer
-                        lenAndId <- getMessage handle
-                        let msg = liftM lenAndIdToMsg lenAndId
-                        case msg of
-                              Just KeepAlive     -> loopAndWait peer "Alive"
-                              Just UnChoke       -> modifyIORef' (amIVirgin peer) (\_->False) >> (print "UNCHOKED")
-                              Just (Bitfield bf) -> modifyIORef' (amIInterested peer) (\_->True) 
-                                                    >> modifyIORef' (bitField peer) (\_->bf)
-                                                    >> (sendMsg handle Interested)
-                                                    >> (talkToPeer peer)
-                              _-> loopAndWait peer (show msg)
-                     else print "Cant talk !!!!!!!!!!!!!!!!!!!!!!!!!!!"
-                     where 
-                        loopAndWait peer m  =  (threadDelay 10000) >> (print m)>>talkToPeer peer
-                        
-
-                     
-sendMsg :: Handle -> Message -> IO ()
-sendMsg handle msg = case (msgToLenAndId msg) of
-                          (x, NoId) -> send $ putWord32be x
-                          (x, Id y) -> send $ putWord32be x >> putWord8 (fromIntegral y)
-                          where 
-                            send = BL.hPutStr handle . runPut  
-          
-          
-getMessage :: Handle -> IO (Maybe (Int, MessageId, B.ByteString))
-getMessage handle = do numBytes <- BL.hGet handle 4
-                       if (BL.length numBytes) < 4 then return Nothing  
-                       else 
-                          do let sizeOfTheBody = (readBEInt numBytes)
-                             case sizeOfTheBody of
-                                  0 -> return $ Just (0, NoId, B.empty) 
-                                  otherwise -> --(Just . swap)<$>(getMsg handle sizeOfTheBody) 
-                                              do 
-                                                 (msgId, body) <- getMsg handle sizeOfTheBody 
-                                                 return $ Just  (sizeOfTheBody, msgId, body)
-                             where  
-                               getMsg handle size = (,)<$>(Id . intFromBS <$> B.hGet handle 1)<*> (B.hGet handle (size -1))
-                               readBEInt = fromIntegral  . runGet getWord32be                            
-
+                         
 
                  
-makePeer :: BC.ByteString -> HostName -> PortNumber -> IO Peer             
-makePeer hash host port = do handle<- connectTo host (PortNumber  port)
-                             hSetBuffering handle NoBuffering                            
+makePeer :: BC.ByteString -> N.HostName -> N.PortNumber -> IO Peer             
+makePeer hash host port = do handle<- N.connectTo host (N.PortNumber  port)
+                             SIO.hSetBuffering handle SIO.NoBuffering                            
                           --   hSetBuffering handle (BlockBuffering Nothing)
                              --hSetBuffering handle LineBuffering
                              sendHandshake handle hash $ BC.pack myId
                              recvHandshake handle                                         
                                
              
-sendHandshake :: Handle -> B.ByteString -> B.ByteString -> IO ()
+sendHandshake :: SIO.Handle -> B.ByteString -> B.ByteString -> IO ()
 sendHandshake handle hash peer = BC.hPutStr handle msg -- >> print "Handshake finished"
         where msg = B.concat [len, ptr, rsrv, hash, peer]
               len = B.singleton $ (fromIntegral . length) protocol
@@ -151,7 +48,7 @@ sendHandshake handle hash peer = BC.hPutStr handle msg -- >> print "Handshake fi
               rsrv = B.replicate 8 (fromIntegral 0)       
               
               
-recvHandshake :: Handle -> IO Peer
+recvHandshake :: SIO.Handle -> IO Peer
 recvHandshake handle = do 
                           len <- B.hGet handle 1
                           ptr <- B.hGet handle $ intFromBS len
