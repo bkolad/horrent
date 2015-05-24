d49fe2e2e1cb826103ef1c0fde44d0880a6a681c
blazej <blazejkolad@gmail.com>
2015-05-24 13:03:48 +0200

    refactoring

--- Connector.hs
+++ Connector.hs
@@ -4,30 +4,23 @@ module Connector ( liftEither, makePeers) where
 
 import qualified Peer as P (Peer, makePeer, showPeer, fromBsToInt) 
 import qualified BencodeParser as BP (BEncode, annouce, infoHash, parseFromFile, parseFromBS, peers, piceSize, torrentSize)
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
 import qualified UrlEncoder as Encoder (urlEncodeVars)
 import qualified Data.Either as DE 
 import qualified Network as N
-import qualified Data.ByteString as B
-import qualified System.IO as SIO
--- import qualified Data.ByteString.Base16 as B16
+import qualified Handshake as H 
 import Data.Binary.Get
 import Data.Word
 import Network.HTTP.Base (urlEncodeVars)
 import Network.HTTP as HTTP
 import Control.Concurrent.Async as Async (mapConcurrently)
-import Control.Exception as E
 import Control.Applicative
 import Control.Monad.Error
 import Types
-import Data.Binary
-import Data.Binary.Get
-import Data.Binary.Put
 
 
-myId = "-TR2840-d0p22uiake0b" 
-protocol = "BitTorrent protocol"
 
 liftEither = ErrorT . return
 
@@ -43,10 +36,10 @@ makePeers tracker numberOfP = do torrentContent <-  liftIO $ BP.parseFromFile tr
                                  peersBS <- liftEither $ ((BP.parseFromBS . BC.pack) resp)  >>= BP.peers
                                  let ipsAndPorts =  getIPandPort peersBS    
                                  globalStatus <- liftIO $ newGlobalBitField numberOfPieces
-                                 handshakes <- liftIO $ Async.mapConcurrently (\(host,port)->getHandshakes infoHash host port) (take numberOfP ipsAndPorts)
+                                 handshakes <- liftIO $ Async.mapConcurrently (\(host,port)->H.getHandshakes infoHash host port) (take numberOfP ipsAndPorts)
                                  let (errorHandshakes, correctHanshakes) = DE.partitionEithers handshakes
                                  liftIO $ print errorHandshakes
-                                 let peers = map (\(handler, handshake) -> P.makePeer handler (peerName handshake) numberOfPieces globalStatus) correctHanshakes
+                                 let peers = map (\(handler, handshake) -> P.makePeer handler (H.peerName handshake) numberOfPieces globalStatus) correctHanshakes
                                  liftIO $ sequence peers
  
                              
@@ -58,7 +51,7 @@ trackerUrl fromDic = do ann <- BP.annouce fromDic
                         vars <- encodedVars <$> (BP.infoHash fromDic)
                         return $ ann++"?"++vars
      where encodedVars hash = Encoder.urlEncodeVars [("info_hash", hash),
-                                                     ("peer_id", myId),
+                                                     ("peer_id", H.myId),
                                                      ("left", "1000000000"),
                                                      ("port", "6881"),
                                                      ("compact", "1"),
@@ -78,62 +71,6 @@ getIPandPort bs = runGet get32and16b (BL.fromChunks [bs])
                                       rest <- get32and16b
                                       return $ (ip, port):rest
              
-             
--- HANDSHAKE --------------------------             
+                         
 
-data Handshake = Handshake { len :: Int 
-                           , peerProtocol :: String
-                           , reserved :: B.ByteString
-                           , hash :: B.ByteString
-                           , peerName :: String
-                           }              
-  
-instance Binary Handshake where  
-  
-  put handshake = putWord8 (fromIntegral . length $ protocol) 
-               >> (putByteString $ BC.pack protocol) 
-               >> (putWord64be 0)
-               >> (putByteString $ hash handshake)
-               >> (putByteString $ BC.pack $ peerName handshake)
-  
-  
-  get = do len <- (P.fromBsToInt <$> getByteString 1)
-           ptr <- BC.unpack <$> getByteString len
-           rsrv <- getByteString 8
-           hash <- getByteString 20
-           peer <- BC.unpack <$> getByteString 20 
-           return $ Handshake len ptr rsrv hash peer                       
-
-
-getHandshakes:: B.ByteString -> N.HostName -> N.PortNumber -> IO (Either String (SIO.Handle, Handshake))           
-getHandshakes hash host port =  E.catch (liftM Right $ handshakes hash host port)
-                                     (\(e::SomeException) -> return . Left $ (show e) ++" "++ (show host))              
-           
-handshakes:: B.ByteString -> N.HostName -> N.PortNumber -> IO (SIO.Handle, Handshake)           
-handshakes hash host port = do handle<- N.connectTo host (N.PortNumber  port)
-                               SIO.hSetBinaryMode handle True
-                               sendHandshake handle hash $ BC.pack myId
-                               handshake <-recvHandshake handle            
-                               return (handle, handshake)
-                                  
-sendHandshake :: SIO.Handle -> B.ByteString -> B.ByteString -> IO ()
-sendHandshake handle hash peer = BL.hPutStr handle $ encode handshake 
-  where handshake = Handshake len protocol rsrv hash myId
-        len = length protocol
-        rsrv = B.replicate 8 0            
-
-{--        
-recvHandshake :: SIO.Handle -> IO Handshake 
-recvHandshake handle =  decode <$> (BL.hGetContents handle)
-
- --}
-
-              
-recvHandshake :: SIO.Handle -> IO Handshake
-recvHandshake handle = do len <- P.fromBsToInt <$>  B.hGet handle 1
-                          ptr <- BC.unpack <$> B.hGet handle len
-                          rsrv <- B.hGet handle 8
-                          hash <- B.hGet handle 20
-                          peer <- BC.unpack <$> B.hGet handle 20 
-                          return $ Handshake len ptr rsrv hash peer
-                                                                      
+    
--- PeersControler.hs
+++ PeersControler.hs
@@ -8,12 +8,8 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Control.Exception as E
 import qualified Data.ByteString.Char8 as BC
-import qualified System.IO as SIO
+import qualified Message as M
 import Control.Concurrent.Async as Async (mapConcurrently)
-import Data.Word
-import Data.Binary
-import Data.Binary.Get
-import Data.Binary.Put
 import Control.Applicative
 import Control.Concurrent
 import Control.Monad
@@ -21,27 +17,6 @@ import Control.Monad.Error
 import Control.Concurrent.STM
 import Data.Array.MArray
 import Data.IORef
-
-type MsgLen = Word32
-
-type MsgPayload = B.ByteString
-
-
-data Message = KeepAlive 
-             | Choke
-             | UnChoke
-             | Interested
-             | NotInterested
-             | Have (Int, B.ByteString)
-             | Bitfield MsgPayload
-             | Request (Int, Int, Int)
-             | Piece (Int, Int, B.ByteString)
-             | Cancel
-             | Port 
-             | Unknown MsgLen Int
-             deriving (Show)
-             
-                            
   
                        
                           
@@ -61,13 +36,13 @@ talkToPeer peer = do --canTalk <- P.canTalkToPeer peer
                      if (True) then do
                         let handle = P.handleP peer
                         print "Waiting"
-                        msg <-  getMessage handle--  liftM lenAndIdToMsg lenAndId
+                        msg <-  M.getMessage handle--  liftM lenAndIdToMsg lenAndId
                         case msg of
-                              KeepAlive     -> loopAndWait peer "Alive" 100000
-                              UnChoke       -> reqNextPice peer
-                              Bitfield bf -> bitfield peer bf
-                              Have (pId, b) ->  have peer pId
-                              p@(Piece (i,b,c)) -> (print $ "got "++(show b))>>piece peer p
+                              M.KeepAlive     -> loopAndWait peer "Alive" 100000
+                              M.UnChoke       -> reqNextPice peer
+                              M.Bitfield bf -> bitfield peer bf
+                              M.Have (pId, b) ->  have peer pId
+                              p@(M.Piece (i,b,c)) -> (print $ "got "++(show b))>>piece peer p
                               _-> loopAndWait peer (show msg) 10000
                      else print "Cant talk !!!!!!!!!!!!!!!!!!!!!!!!!!!"
                      where 
@@ -78,23 +53,23 @@ reqNextPice :: P.Peer -> IO ()
 reqNextPice peer = do nextLs <- P.nextPiceToRequest peer
                       case nextLs of
                            []        -> print "Finisched"
-                           (x, b):xs -> (print "req")>>(sendMsg peer $ Request (10, 0* 16384, 16384))
+                           (x, b):xs -> (print "req")>>(M.sendMsg peer $ M.Request (10, 0* 16384, 16384))
                                             >> print ("Request "++ (show x))
                                             >> talkToPeer peer
                    
                    
 
 --piece :: P.Peer -> IO ()
-piece peer (Piece (i,b,c)) = do P.appendToBuffer peer c
-                                if (next>= 32*16384)
-                                then (P.appendBuffToFile peer (show i)) -- >>reqNextPice peer
-                                else (print ("Request subPice "++ (show next)))>>(sendMsg peer $ Request (i, next, 16384))
-                                           >> talkToPeer peer
-                              where next = b+16384
+piece peer (M.Piece (i,b,c)) = do P.appendToBuffer peer c
+                                  if (next>= 32*16384)
+                                  then (P.appendBuffToFile peer (show i)) -- >>reqNextPice peer
+                                  else (print ("Request subPice "++ (show next)))>>(M.sendMsg peer $ M.Request (i, next, 16384))
+                                            >> talkToPeer peer
+                               where next = b+16384
                               
                    
                    
-bitfield :: P.Peer -> MsgPayload -> IO()
+bitfield :: P.Peer -> M.MsgPayload -> IO()
 bitfield peer bf = P.updateBF peer bf
                    >> sendInterested peer
                    >> print "Got BF" >> (talkToPeer peer)
@@ -105,65 +80,6 @@ have peer pId = P.updateBFIndex peer pId    -- Interested?
                 >> sendInterested peer
                 >> print "have" >> (talkToPeer peer)
  
-sendInterested peer =  (sendMsg peer Interested)
+sendInterested peer =  (M.sendMsg peer M.Interested)
  
-   
-instance Binary Message where
   
-   put KeepAlive           = putWord32be 0
-   put Choke               = putWord32be 1 >> putWord8 0
-   put UnChoke             = putWord32be 1 >> putWord8 1
-   put Interested          = putWord32be 1 >> putWord8 2
-   put NotInterested       = putWord32be 1 >> putWord8 3
-   put (Request (i, o, l)) = putWord32be 13 >> putWord8 6 >> put32Int i 
-                                                          >> put32Int o 
-                                                          >> put32Int l
-                                 
-       where  put32Int = putWord32be . fromIntegral    
-   
-   
-   put (Piece _)           = undefined
-   put Cancel              = putWord32be 13 >> putWord8 8
-   put Port                = putWord32be 3 >> putWord8 9
-   put (Unknown _ _)       = undefined 
-   
-   
-   
-   get = do numBytes <- fromIntegral <$> getWord32be
-            if (numBytes == 0)
-               then return KeepAlive
-               else do idx <- getWord8
-                       let size = numBytes -1  
-                       bs <- getByteString size
-                       case idx of
-                             0 -> return Choke
-                             1 -> return UnChoke
-                             2 -> return Interested
-                             3 -> return NotInterested
-                             4 -> return $ Have (P.fromBsToInt bs, bs)
-                             5 -> return $ Bitfield bs
-                             6 -> return $ Request (undefined, undefined, undefined)
-                             7 -> return $ Piece (toPiece bs)
-                             8 -> return $ Cancel
-                             9 -> return $ Port
-                             x -> return $ Unknown (fromIntegral size) (fromIntegral x)
-       
-       
-toPiece bs = runGet getTripplet (BL.fromChunks [bs])
-  where getTripplet :: Get (Int, Int, B.ByteString)
-        getTripplet = do idx <-  getWord32be
-                         begin <- getWord32be
-                         rest <- getRemainingLazyByteString
-                         return $ (fromIntegral idx, fromIntegral begin, (B.concat . BL.toChunks) rest)
-   
-   
-getMessage :: SIO.Handle -> IO Message 
-getMessage handle =  decode <$> (BL.hGetContents handle)
-                                                         
-                  
-sendMsg :: P.Peer -> Message -> IO ()
-sendMsg peer msg = send msg
-  where handle = P.handleP peer
-        send = BL.hPutStr handle . encode  
-                          
-                 
