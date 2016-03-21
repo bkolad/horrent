5cebe2085faa8d96721d81170b4ae7a3a15965aa
--list <blazejkolad@gmail.com>
2016-03-21 21:13:15 +0100

    Acton DSL udated

---
+++ app/Action.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module Action (ActionF (SendInterested
+                          , Log
+                          , ReqNextAndUpdate
+                          , SendRequest
+                          , SetStatus)
+                          , setStatusF
+                          , sendInterestedF
+                          , logF
+                          , requestNextAndUpdateGlobalF
+                          , sendRequestF
+                          , Action
+                          , Free(Free,Pure)) where
+
+import Control.Monad.Free (Free(Free,Pure))
+import qualified Types as TP
+
+data ActionF a = SendInterested a
+                | Log String a
+                | ReqNextAndUpdate [Int] TP.GlobalPiceInfo ((Maybe Int) -> a)
+                | SendRequest (Int, Int, Int) a
+                | SetStatus Int TP.GlobalPiceInfo TP.PiceInfo a
+                deriving (Functor)
+
+
+type Action = Free ActionF
+
+sendInterestedF :: Action ()
+sendInterestedF = Free $ SendInterested (Pure ())
+
+
+logF :: String -> Action ()
+logF str = Free $ Log str (Pure ())
+
+
+requestNextAndUpdateGlobalF :: [Int]
+                            -> TP.GlobalPiceInfo
+                            -> Action (Maybe Int)
+requestNextAndUpdateGlobalF pieces global =
+    Free $ ReqNextAndUpdate pieces global Pure
+
+
+sendRequestF :: (Int, Int, Int) -> Action ()
+sendRequestF req = Free $ SendRequest req (Pure ())
+
+
+setStatusF :: Int -> TP.GlobalPiceInfo -> TP.PiceInfo -> Action ()
+setStatusF x globab status = Free $ SetStatus x globab status (Pure ())
