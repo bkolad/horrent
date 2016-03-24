024a3b4006a8cc9c916bfec5a3978ea1fd168f4a
--list <blazejkolad@gmail.com>
2016-03-25 00:55:26 +0100

    Simulator

--- app/InterpretST.hs
+++ app/InterpretST.hs
@@ -1,6 +1,11 @@
 module InterpretST where
 
 import Control.Monad.State.Lazy
+import Data.Conduit
+import qualified Message as M
+import qualified Data.ByteString.Char8 as BC
+import qualified TubeDSL as TDSL
+
 
 import Action
 
@@ -10,6 +15,7 @@ data ActionST = GotInterested Bool
               | GotReqNext
               | GotSendReq
               | GotSetStatus
+              deriving Show
 
 -- TODO write conduit pipe with ActionST state
 
@@ -19,7 +25,13 @@ interpret program =
     case program of
         Free (SendInterested c) -> undefined
 
-        Free (Log str c) -> undefined
+        Free (Log str c) -> do
+            st <- get
+            case st of
+                GotLog x -> do
+                    put (GotLog $ str:x)
+                    interpret c
+
 
         Free (ReqNextAndUpdate pieces fun) -> undefined
 
@@ -28,3 +40,23 @@ interpret program =
         Free (SetStatus x status c) -> undefined
 
         Pure x -> return x
+
+
+networkSource :: Source (State ActionST) M.Message
+networkSource = yield M.KeepAlive
+
+fileSink ::  Sink (String, BC.ByteString) (State ActionST) ()
+fileSink = do
+    x <- await
+    case x of
+        Nothing -> return ()
+        Just s -> fileSink
+
+
+testTube :: State ActionST ()
+testTube = networkSource
+     $= (transPipe interpret (TDSL.recMessage undefined))
+     $$ fileSink
+
+
+test = runState testTube (GotLog [])
--- app/Peer.hs
+++ app/Peer.hs
@@ -6,7 +6,7 @@ import qualified Network as N
 import qualified Types as TP
 import qualified Data.Word as W
 import qualified Data.List as L
-import qualified Crypto.Hash.SHA1 as SHA1 
+import qualified Crypto.Hash.SHA1 as SHA1
 
 
 data Peer = Peer { hostName :: N.HostName
@@ -15,65 +15,69 @@ data Peer = Peer { hostName :: N.HostName
                  , infoHash :: B.ByteString
                  , globalStatus :: TP.GlobalPiceInfo
                  , unChoked :: Bool
-                 , buffer :: B.ByteString 
+                 , buffer :: B.ByteString
                  , peceHashes :: TP.HashInfo
                  , sizeInfo :: (TP.NumberOfPieces, TP.NormalPieceSize, TP.LastPieceSize)
                  }
 
-                 
+
 instance Show Peer where
-  show p = (hostName p) ++ " "++ (show $ port p) ++ " " ++ (show $ pieces p)                
-                 
-                 
-                 
-fromBsToInt bs = 
+  show p = (hostName p) ++ " "++ (show $ port p) ++ " " ++ (show $ pieces p)
+
+
+
+fromBsToInt bs =
    sum $ zipWith (\x y->x*2^y) (reverse ws) [0,8..]
-   where 
+   where
       ws = map fromIntegral (B.unpack bs)
 
-      
-      
-convertToBits :: B.ByteString -> [Int]      
-convertToBits bs = 
-   map snd $ filter fst $ zip bits [0 ..] 
+
+convertBitsToBs :: [Int] ->  B.ByteString
+convertBitsToBs ls = let l = l8 ls
+                         lz = map (zip [7,6 ..0 ]) l
+                         lzz = map (filter (\(i, x)-> x/=0)) lz
+                         lzzz = map (map (\(i,x)->2^i)) lzz
+                         lk = map (sum) lzzz
+                     in B.pack $ map (fromInteger)lk
+
+l8 :: [a] -> [[a]]
+l8 [] = []
+l8 ls = let (l, r) = splitAt 8 ls
+        in l : l8 r
+
+convertToBits :: B.ByteString -> [Int]
+convertToBits bs =
+   map snd $ filter fst $ zip bits [0 ..]
    where
      bits = [Bits.testBit w i| w <- B.unpack bs, i <- [7,6.. 0]]
-     
-     
-     
-     
-hashFor = SHA1.hash  
-  
-     
+
+
+
+
+hashFor = SHA1.hash
+
+
 intsToBools :: [Int] -> Int -> [Bool]
 intsToBools [] idx = []
 intsToBools (x : xs) idx = if (idx == x)
                               then True : (intsToBools xs (idx+1))
                               else False : (intsToBools (x:xs) (idx+1))
 
-groupBools [] = [[]] 
+groupBools [] = [[]]
 groupBools ls = let (ls8, rest) = L.splitAt 8 ls
                 in ls8 : (groupBools rest)
- 
-                              
-                              
+
+
+
 boolsToW8 :: [Bool] -> W.Word8
 boolsToW8 ls = L.foldl'(\acc (i, b)-> acc + (if b then (2^ (7-i)) else 0) ) 0 (zip [0 ..] ls)
-    
+
 
 hasIdxs :: [Int] -> B.ByteString
-hasIdxs ls = B.pack $ init $  map boolsToW8 $ groupBools (intsToBools ls 0)     
-    
-    
-    
-ls = [True, False, False, False, False, False, False, True]  
-  
-kk = intsToBools [1,6,7] 0 
-
-
-        
+hasIdxs ls = B.pack $ init $  map boolsToW8 $ groupBools (intsToBools ls 0)
 
 
 
-  
-      
+ls = [True, False, False, False, False, False, False, True]
+
+kk = intsToBools [1,6,7] 0
