07f66463b6df9cb2a27008848374899449c2cbab
--list <blazejkolad@gmail.com>
2016-08-07 12:48:31 +0200

    logger

--- src/Logger/BasicLogger.hs
+++ src/Logger/BasicLogger.hs
@@ -13,6 +13,8 @@ import Control.Concurrent (forkIO)
 import Control.Monad.STM (atomically)
 import Control.Monad (forever)
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Writer.Class
 
 data BasicLogger =
     BasicLogger { unlogger :: TChan T.Text }
@@ -35,70 +37,77 @@ logString l msg = logMsg l (T.pack msg)
 process :: TChan T.Text -> IO ()
 process chan = forever $ do
     l <- atomically $ readTChan chan
+    T.putStrLn "BL!!!!!!"
     T.putStrLn l
 
 
 
-data Handle = H
-            | G
-            deriving Show
+data LoggerT h m a = L { runLogger:: h -> m a }
 
 
-data LoggerT m a = L { runLogger:: Handle -> m a }
-
-instance (Monad m) => Functor (LoggerT m) where
+instance (Monad m) => Functor (LoggerT h m) where
     fmap f (L fun) = L $
       \h -> do x <- fun h
                return $ f x
 
-instance (Monad m) => Applicative (LoggerT m) where
+
+instance (Monad m) => Applicative (LoggerT h m) where
     pure x = L $ \h -> return x
     (L fo) <*> (L x) = L $ \h -> (fo h) <*> (x h)
 
 
-instance (Monad m) => Monad (LoggerT m) where
+instance (Monad m) => Monad (LoggerT h m) where
     return = pure
     (L x) >>= fx = L $ \h ->
         do k <- (x h)
            runLogger (fx k) h
 
 
-printT h = print ("blabal " ++ show h)
-printT2 h = print ("blabal222 " ++ show h)
 
-test :: LoggerT IO ()
-test =  do L printT
-           L printT2
 
-tt = runLogger test H
-
+class Logable a where
+   logM :: a -> T.Text -> IO ()
 
 
 class (Monad m) => MonadLogger m where
     logMessage :: T.Text -> m ()
 
+
+instance (Logable l) => MonadTrans (LoggerT l) where
+    lift m = L $ \h -> m
+
+
+instance (Logable l, MonadIO m) => MonadIO (LoggerT l m) where
+    liftIO = lift . liftIO
+
+
+instance (Logable l, MonadIO m) => MonadLogger (LoggerT l m) where
+    logMessage x = L $ \h -> liftIO $ logM h x
+
+
 instance MonadLogger IO where
     logMessage x = T.putStrLn x
 
 
-instance (MonadIO m) => MonadLogger (LoggerT m) where
-    logMessage x = L $ \h -> liftIO $ printT h
+-- =================
 
-instance (MonadIO m) => MonadIO (LoggerT m) where
-    liftIO = undefined--lift . liftIO
+instance Logable BasicLogger where
+    logM = logMsg
+
+
+
+-- ===============
+
 
 xx :: (MonadIO m, MonadLogger m) => m Int
 xx = do
-    --liftIO $ print "lalal"
-    logMessage "oooo"
+    liftIO $ print "lalal"
+    logMessage "oooouuuu"
     return 2
 
 
+
 yy :: IO Int
-yy = runLogger xx H
-
-
-{--
-
-instance (Monad m) => MonadLogger l (LoggerT l m) where
-    logMessage x = undefined --}
+yy = do
+    bl <- start
+    runLogger xx bl
